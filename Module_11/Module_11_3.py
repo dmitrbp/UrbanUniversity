260bc919eb4f3ca144ba2796ada9a6e72a4c1fef
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-08-22 18:24:45 +0300

    release 0

--- Module_11/Module_11_3.py
+++ Module_11/Module_11_3.py
@@ -1,5 +1,6 @@
 import inspect
 
+
 def introspection_info(obj):
     result = {}
     result['type'] = type(obj).__name__
@@ -7,6 +8,7 @@ def introspection_info(obj):
     result['methods'] = [name for name, value in inspect.getmembers(obj) if callable(value)]
     result['module'] = inspect.getmodule(introspection_info).__name__
     return result
+
 
 number_info = introspection_info(42)
 print(number_info)
