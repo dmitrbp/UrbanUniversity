304758d2ba4b02d84728f3b4a52cd3e320349b05
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-09-04 19:10:30 +0300

    release 0

---
+++ Module_13/Module_13_3.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '7444786549:AAGQqDYFQpApu_qBoqhp7u_481xv3Ca7iF4'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_messages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
