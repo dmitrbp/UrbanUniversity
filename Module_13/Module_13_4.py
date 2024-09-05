04b4a3743d1b53766d7e57767c5452e051e4f759
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-09-06 02:28:18 +0300

    release 0

--- Module_13/Module_13_4.py
+++ Module_13/Module_13_4.py
@@ -7,7 +7,7 @@ api = key
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-api = '7444786549:AAGQqDYFQpApu_qBoqhp7u_481xv3Ca7iF4'
+api = key
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -15,4 +15,32 @@ class UserState(State):
     age = State()
     growth = State()
     weight = State()
+
+@dp.message_handler(text = 'Calories')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state = UserState.address)
+async def send_colories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    calories = data['weight'] * 10 +  data['growth'] * 6,25 + data['age'] * 5 + 5
+    await message.answer(f"Калорий: {calories}")
+    await state.finish()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
 
