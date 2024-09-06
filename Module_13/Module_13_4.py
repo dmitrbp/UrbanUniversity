0b1fdb20f54b4181968da8eaa5a515c420bf4bdd
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-09-06 03:16:11 +0300

    release 0

--- Module_13/Module_13_4.py
+++ Module_13/Module_13_4.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
