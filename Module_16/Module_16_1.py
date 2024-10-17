247fe23043cbabde0b185ef67008721b960ac800
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-10-17 15:43:03 +0300

    release 0

---
+++ Module_16/Module_16_1.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+async def root_route() -> str:
+    return 'Главная страница'
+
+@app.get('/user/admin')
+async def path_route() -> str:
+    return 'Вы вошли как администратор'
+
+@app.get('/user/{user_id}')
+async def parameter_route(user_id) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
+
+@app.get('/user')
+async def query_route(username = 'NoName', age = 'NoAge') -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8001)
