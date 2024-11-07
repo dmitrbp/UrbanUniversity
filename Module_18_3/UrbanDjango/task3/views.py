43f64496c28e79986850b9decb805c3ed8cd3ac9
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-11-07 18:25:50 +0300

    release 0

--- Module_18_3/UrbanDjango/task3/views.py
+++ Module_18_3/UrbanDjango/task3/views.py
@@ -11,7 +11,21 @@ def platform_view(request):
     return render(request, 'platform.html', context)
 
 def games_view(request):
-    return render(request, 'games.html')
+    title = 'Игры'
+    context = {
+        'title': title,
+        'game1': 'Atomic Heart',
+        'game2': 'Cyberpunk 2077',
+        'game3': 'PayDay 2'
+    }
+    return render(request, 'games.html', context)
 
 def cart_view(request):
-    return render(request, 'cart.html')
+    title = 'Корзина'
+    context = {
+        'title': title,
+        'pos1': 'Diablo III',
+        'pos2': 'Minecraft',
+        'pos3': 'Half-Life 2'
+    }
+    return render(request, 'cart.html', context)
