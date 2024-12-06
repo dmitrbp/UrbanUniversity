8a246a9a35eed5fa369e11ab875346bf15e13d30
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-12-06 13:40:31 +0300

    release 0

--- Diploma/Practice/filewrite_classes.py
+++ Diploma/Practice/filewrite_classes.py
@@ -4,20 +4,28 @@ import threading
 import time
 import aiofiles
 from async_class import AsyncClass
+from collections import namedtuple
 
+
+
+TestResult = namedtuple('TestResult', ['file_size', 'elapsed_time', 'threads_count'])
 
 class AsyncTester(AsyncClass):
     async def __ainit__(self, files_count, file_size):
         """
-
-        :param files_count:
-        :param file_size:
-        :return:
+        Конструктор класса
+        :param files_count: количество файлов для тестирования
+        :param file_size: размер каждого файлов в байтах
+        :return: класс
         """
         self.files_count = files_count
         self.file_size = file_size
 
     async def run(self):
+        """
+        Функция асинхронного тестирования
+        :return: результаты тестирования в виде именованного кортежа
+        """
         write_tasks = []
         read_tasks = []
         start_time = time.time()
@@ -28,15 +36,24 @@ class AsyncTester(AsyncClass):
         threads_count = threading.active_count()
         await asyncio.gather(*write_tasks)
         await asyncio.gather(*read_tasks)
-        return self.file_size, (time.time() - start_time), threads_count
+        return TestResult(self.file_size, (time.time() - start_time), threads_count)
 
     async def writter(self, filename):
+        """
+        Функция записи в файл
+        :param filename: имя файла
+        :return: None
+        """
         async with aiofiles.open(filename, mode='w') as file:
             content = ''.join([str(num) for num in range(self.file_size)])
             await file.write(content)
 
-    @staticmethod
-    async def reader(files_count):
+    async def reader(self, files_count):
+        """
+        Функция чтения из случайного файла в пределах предварительно созданных
+        :param files_count: количество файлов
+        :return: None
+        """
         i = random.randint(0, files_count - 1)
         async with aiofiles.open(f'{i}.txt', mode='r') as file:
             await asyncio.sleep(random.uniform(0, 2))
@@ -45,10 +62,19 @@ class AsyncTester(AsyncClass):
 
 class ThreadTester:
     def __init__(self, files_count, file_size):
+        """
+        Конструктор класса
+        :param files_count: количество файлов
+        :param file_size: размер каждого файла в байтах
+        """
         self.files_count = files_count
         self.file_size = file_size
 
     def run(self):
+        """
+        Функция потокового тестирования
+        :return: результаты тестирования в виде именованного кортежа
+        """
         write_threads = []
         read_threads = []
         start_time = time.time()
@@ -68,15 +94,24 @@ class ThreadTester:
         threads_count = max(threads_count, threading.active_count())
         for thread in read_threads:
             thread.join()
-        return self.file_size, (time.time() - start_time), threads_count
+        return TestResult(self.file_size, (time.time() - start_time), threads_count)
 
     def writter(self, i):
+        """
+        Функция записи в файл
+        :param i: номер файла
+        :return: None
+        """
         with open(f'{i}.txt', mode='w') as file:
             content = ''.join([str(num) for num in range(self.file_size)])
             file.write(content)
 
-    @staticmethod
-    def reader(files_count):
+    def reader(self, files_count):
+        """
+        Функция чтения из случайного файла в пределах предварительно созданных
+        :param files_count: количество файлов
+        :return:
+        """
         i = random.randint(0, files_count - 1)
         with open(f'{i}.txt', mode='r') as file:
             time.sleep(random.uniform(0, 2))
--- Diploma/Practice/filewrite_test.py
+++ Diploma/Practice/filewrite_test.py
@@ -1,9 +1,16 @@
 import asyncio
 import matplotlib.pyplot as plt
+from filewrite_classes import *
+from collections import namedtuple
 
-from filewrite_classes import AsyncTester, ThreadTester
 
 async def async_tester(files_count, file_size):
+    """
+    Функция одиночного асинхронного тестирования
+    :param files_count: количество файлов
+    :param file_size: размер каждого файла
+    :return:
+    """
     writter = await AsyncTester(files_count, file_size)
     result = await writter.run()
     print (
@@ -14,6 +21,12 @@ async def async_tester(files_count, file_size):
     return result
 
 def thread_tester(files_count, file_size):
+    """
+    Функция одиночного потокового тестирования
+    :param files_count: количество файлов
+    :param file_size: размер каждого файла
+    :return:
+    """
     writter = ThreadTester(files_count, file_size)
     result = writter.run()
     print (
@@ -23,16 +36,20 @@ def thread_tester(files_count, file_size):
     )
     return result
 
+# Количество файлов для тестирования
 files_count = 10
+# Списки для построения графиков
 x = []
 y1 = []
 y2 = []
+# Цикл тестирования для разных размеров файлов
 for f_size in range(100000, 200001, 25000):
     async_result = asyncio.run(async_tester(files_count, f_size))
     thread_result = thread_tester(files_count, f_size)
     x.append(f_size / 1000000)
-    y1.append(async_result[1])
-    y2.append(thread_result[1])
+    y1.append(async_result.elapsed_time)
+    y2.append(thread_result.elapsed_time)
+# Отображение результатов на графике
 plt.title('Сравнение времени записи/чтения файлов для AsyncIO и Threads')
 plt.xlabel('Размер файла (Mb)')
 plt.ylabel('Время выполнения (сек)')
